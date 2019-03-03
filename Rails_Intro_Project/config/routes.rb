fd54ddc994bda861c1e46f41b47468dce67bb0d5
Michelle <michelle.aldn@outlook.com>
2019-03-03 00:42:06 -0600

    Added the books and search controllers.

--- Rails_Intro_Project/config/routes.rb
+++ Rails_Intro_Project/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'search/results'
+  get 'books/index'
+  get 'books/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
